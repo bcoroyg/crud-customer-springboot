90b8a509bb4da95a74d46de49e265c2f884f4ccc
Emilio Coroy <becgonzalez25@gmail.com>
2023-01-17 19:22:49 -0600

    Creando Entities y Repositories con JPA

---
+++ src/main/java/com/example/apirest/models/Customer.java
@@ -0,0 +1,34 @@
+package com.example.apirest.models;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100)
+    private String firstname;
+
+    private String lastname;
+
+    @Temporal(TemporalType.DATE)
+    private Date createdAt;
+}
