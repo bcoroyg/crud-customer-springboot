68972601c06059e171638fc3f17bdfe16cf1ce81
Emilio Coroy <becgonzalez25@gmail.com>
2023-01-17 20:23:31 -0600

    Controller update customer

--- src/main/java/com/example/apirest/controllers/CustomerController.java
+++ src/main/java/com/example/apirest/controllers/CustomerController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -65,5 +66,34 @@ public class CustomerController {
         }
 
         return new ResponseEntity<Customer>(customer, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateCustomer(@PathVariable Long id, @RequestBody Customer customer) {
+        Customer customerDB = service.findById(id);
+        Customer customerEdit = null;
+
+        Map<String, Object> response = new HashMap<>();
+
+        if (customerDB == null) {
+            response.put("message", "El cliente con ID:".concat(id.toString()).concat(" no existe."));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+
+        try {
+            customerDB.setFirstname(customer.getFirstname());
+            customerDB.setLastname(customer.getLastname());
+
+            customerEdit = service.save(customerDB);
+        } catch (DataAccessException e) {
+            response.put("error", e.getMessage());
+            response.put("message", "Error al actualizar un cliente");
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("message", "Usuario actulizado con exito");
+        response.put("customer", customerEdit);
+
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
     }
 }
